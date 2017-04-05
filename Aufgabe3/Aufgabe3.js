f28435a8431469ba76ed9ed27ff36e3329a5b5e8
Bersu <Bersu@Bersu-PC.fritz.box>
2017-04-06 01:05:04 +0200

    Grundgerüst hochgeladen

---
+++ Aufgabe3/Aufgabe3.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    var rice = 1;
+    var line = 0;
+    var i = 0;
+    for (var i_1 = 0; i_1 < 64; i_1++) {
+        var element = document.createElement("div");
+        if (((i_1 + line) % 2) == 0) {
+            element.className = "board white";
+        }
+        else {
+            element.className = "board black";
+        }
+        if (((i_1 + 1) % 8) == 0) {
+            line++;
+        }
+        element.innerText = " " + rice;
+        rice *= 2;
+        document.body.appendChild(element);
+    }
+};
+//# sourceMappingURL=Aufgabe3.js.map
---
+++ Aufgabe3/Aufgabe3.ts
@@ -0,0 +1,24 @@
+window.onload = function (): void {
+ 
+    let rice: number = 1 ; 
+    let line: number = 0 ; 
+    let i: number = 0; 
+   
+    for (let i: number = 0; i < 64; i++) {
+        let element: HTMLElement = document.createElement("div");
+
+        if (((i + line) % 2) == 0) {
+            element.className = "board white";
+            }
+        else {
+            element.className = "board black";
+            }
+        if (((i + 1) % 8) == 0) {
+            line++; }
+        
+        element.innerText = " " + rice;
+        rice *= 2;
+        document.body.appendChild(element);
+        }
+    };
+        
