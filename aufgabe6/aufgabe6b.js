2ffe4edbfea855d3d2dd5c6f862d6ca50a56ed2c
Bersu <Bersu@Bersu-PC.fritz.box>
2017-05-07 15:12:36 +0200

    6b hochgeladen

--- aufgabe6/aufgabe6b.js
+++ aufgabe6/aufgabe6b.js
@@ -1,3 +1,7 @@
+/*Aufgabe: Nr. 6b
+Name: Bersu Inanir
+Datum: 07.05.2017
+In Zusammenarbeit mit Lisa Würstle */
 var StudiVZ;
 (function (StudiVZ) {
     var students = [];
@@ -7,7 +11,7 @@ var StudiVZ;
         switch (action) {
             case "n":
             case "N":
-                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 für M und 1 für W) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -21,10 +25,37 @@ var StudiVZ;
         }
     }
     function saveData(_input) {
-        return "Hier fehlt noch der richtige Code...";
+        var datenArray = _input.split(",");
+        var s = {
+            matrikelnummer: parseInt(datenArray[0]),
+            name: datenArray[1],
+            vorname: datenArray[2],
+            alter: parseInt(datenArray[3]),
+            geschlecht: parseInt(datenArray[4]) == 1,
+            kommentar: datenArray[5]
+        };
+        students.push(s);
+        var geschlecht;
+        if (parseInt(datenArray[4]) == 1) {
+            // 1 = weiblich
+            geschlecht = "weiblich";
+        }
+        else {
+            geschlecht = "männlich";
+        }
+        return "Ihre Eingabe wurde gespeichert:" + "\n" + "\n MATRIKELNUMMER: " + s.matrikelnummer + "\n NAME: " + s.name + "\n VORNAME: " + s.vorname + "\n ALTER: " + s.alter + "\n GESCHLECHT: " + geschlecht + "\n KOMMENTAR: " + s.kommentar;
     }
     function queryData(_matrikel) {
-        return "Hier fehlt noch der richtige Code...";
+        var i = 0;
+        for (i = 0; i < students.length; i++) {
+            if (_matrikel == students[i].matrikelnummer) {
+                var geschlecht = students[i].geschlecht ? "female" : "male";
+                //Ausgabe wenn es einen Treffer erzielt
+                return "Ihre Suche in StudiVZ ergab folgenden Treffer:" + "\n" + "\n MATRIKELNUMMER: " + students[i].matrikelnummer + "\n NAME: " + students[i].name + "\n VORNAME: " + students[i].vorname + "\n ALTER: " + students[i].alter + "\n GESCHLECHT: " + geschlecht + "\n KOMMENTAR: " + students[i].kommentar;
+            }
+        }
+        //Ausgabe wenn es keinen Treffer erzielt
+        return "Matrikelnummer ist nicht vorhanden. Überprüfen Sie bitte Ihre Angaben!";
     }
 })(StudiVZ || (StudiVZ = {}));
 //# sourceMappingURL=aufgabe6b.js.map
--- aufgabe6/aufgabe6b.ts
+++ aufgabe6/aufgabe6b.ts
@@ -1,6 +1,19 @@
+/*Aufgabe: Nr. 6b
+Name: Bersu Inanir
+Datum: 07.05.2017
+In Zusammenarbeit mit Lisa Würstle */
+
+
 namespace StudiVZ {
     interface StudentData {
         // hier ist noch die richtige Datenstruktur festzulegen
+
+        matrikelnummer: number;
+        name: string;
+        vorname: string;
+        alter: number;
+        geschlecht: boolean;
+        kommentar: string;
     }
     var students: StudentData[] = [];
     var stop: boolean = false;
@@ -11,7 +24,7 @@ namespace StudiVZ {
         switch (action) {
             case "n":
             case "N":
-                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 für M und 1 für W) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -26,9 +39,40 @@ namespace StudiVZ {
     }
 
     function saveData(_input: string): string {
-        return "Hier fehlt noch der richtige Code...";
+        let datenArray: string[] = _input.split(",");
+        let s: StudentData = {
+            matrikelnummer: parseInt(datenArray[0]),
+            name: datenArray[1],
+            vorname: datenArray[2],
+            alter: parseInt(datenArray[3]),
+            geschlecht: parseInt(datenArray[4]) == 1,
+            kommentar: datenArray[5]
+
+        };
+        students.push(s);
+
+        let geschlecht: string;
+        if (parseInt(datenArray[4]) == 1) {   // DatenArray[4] = 4. Schublade
+                                               // 1 = weiblich
+            geschlecht = "weiblich";
+        }
+        else {                      // 0 = männlich 
+            geschlecht = "männlich";
+        }
+
+        return "Ihre Eingabe wurde gespeichert:" + "\n" + "\n MATRIKELNUMMER: " + s.matrikelnummer + "\n NAME: " + s.name + "\n VORNAME: " + s.vorname + "\n ALTER: " + s.alter + "\n GESCHLECHT: " + geschlecht + "\n KOMMENTAR: " + s.kommentar;
     }
+
     function queryData(_matrikel: number): string {
-        return "Hier fehlt noch der richtige Code...";
+        let i: number = 0;
+        for (i = 0; i < students.length; i++) { //durchsuchen der gespeicherten Daten
+            if (_matrikel == students[i].matrikelnummer) {
+                let geschlecht: string = students[i].geschlecht ? "female" : "male";
+                //Ausgabe wenn es einen Treffer erzielt
+                return "Ihre Suche in StudiVZ ergab folgenden Treffer:" + "\n" + "\n MATRIKELNUMMER: " + students[i].matrikelnummer + "\n NAME: " + students[i].name + "\n VORNAME: " + students[i].vorname + "\n ALTER: " + students[i].alter + "\n GESCHLECHT: " + geschlecht + "\n KOMMENTAR: " + students[i].kommentar;
+            }
+        }
+        //Ausgabe wenn es keinen Treffer erzielt
+        return "Matrikelnummer ist nicht vorhanden. Überprüfen Sie bitte Ihre Angaben!";
     }
 }
